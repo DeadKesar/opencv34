7aae2a2ac18cb245cdd7f127ac40aed8b73f3797
DeadKesar <50805926+DeadKesar@users.noreply.github.com>
2024-11-20 09:19:14 +0300

    Добавьте файлы проекта.

---
+++ opencv34/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Tesseract;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        string tesseractDataPath = @"D:\Program Files\Tesseract-OCR\tessdata"; // Папка с языковыми файлами
+        string imagePath = @"D:\4\5.jpg"; // Путь к изображению
+
+        // Убедитесь, что языковые файлы (например, eng.traineddata) находятся в tessdata
+        Console.WriteLine("Начинаем распознавание текста...");
+
+        try
+        {
+            using (var engine = new TesseractEngine(tesseractDataPath, "rus", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(imagePath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        string text = page.GetText();
+                        Console.WriteLine("Распознанный текст:");
+                        Console.WriteLine("--------------------");
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
